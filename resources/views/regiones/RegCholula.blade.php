c5de0eabf58e2296d68180dcb9a2b778873738cc
CHRISTIAN-URIEL <utp0148208@alumno.utpuebla.edu.mx>
2025-03-14 17:33:14 -0600

    variables de color

--- resources/views/regiones/RegCholula.blade.php
+++ resources/views/regiones/RegCholula.blade.php
@@ -25,7 +25,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#inicio">Inicio</a>
+                        <a class="nav-link" href="#region">Inicio</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#atracciones">Atracciones</a>
