d46c57e19c10a3f509db7501a9e07451bba77497
CHRISTIAN-URIEL <utp0148208@alumno.utpuebla.edu.mx>
2025-03-20 11:06:17 -0600

    elimimacion de menu desplegable (regiones)

--- resources/views/regiones/RegCholula.blade.php
+++ resources/views/regiones/RegCholula.blade.php
@@ -40,24 +40,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#restaurantes">Dónde Comer</a>
                     </li>
-                    <!--
-                    Menú desplegable de regiones
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownRegiones" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Regiones
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownRegiones">
-                            <li><a class="dropdown-item" href="#">Puebla</a></li>
-                            <li><a class="dropdown-item" href="{{ route('regiones.RegCholula') }}">Cholula</a></li>
-                            <li><a class="dropdown-item" href="#">Atlixco</a></li>
-                            <li><a class="dropdown-item" href="{{route('regiones.RegZacatlan')}}">Zacatlán</a></li>
-                            <li><a class="dropdown-item" href="#">Sierra Oriental</a></li>
-                        </ul>
-                    </li>
-                    Fin del menú desplegable de regiones
-                </ul>
-                <a href="{{ route('ceydt.index') }}" class="btn btn-brand ms-lg-3">Cluster</a> -->
             </div>
         </div>
     </nav>
